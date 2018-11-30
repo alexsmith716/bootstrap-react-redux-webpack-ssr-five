5e728707a15dca7ad1081ebe9d8fc3df5d9b35ec
AlexSmith716 <alexsmith716@outlook.com>
2018-11-30 15:48:24 -0500

    refactor(createStore): updated connected-react-router to v5 and made initial migration code changes.

--- client/redux/reducer.js
+++ client/redux/reducer.js
@@ -1,15 +1,16 @@
+import { connectRouter } from 'connected-react-router';
 import auth from './modules/auth';
 import notifs from './modules/notifs';
 import info from './modules/info';
 
-export default function createReducers(asyncReducers) {
-
+// root reducer
+export default function createReducers(history, asyncReducers) {
   return {
+  	router: connectRouter(history),
     online: (v = true) => v,
     notifs,
     auth,
     info,
     ...asyncReducers,
   };
-
 }
